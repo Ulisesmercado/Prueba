cad1a23f3d53ffe4c154aa134cbfe0fcd2b1435b
ulises <umsalex@gmail.com>
2016-07-19 13:48:28 -0500

    se subio el index como medio de prueba se subio una prueba mas

---
+++ Index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "hola que tal";
+echo "creo que ya esta a punto de funcionar";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>CLIENTE FRECUENTE</title>
+</head>
+<body>
+</body>
+</html>
+
+
